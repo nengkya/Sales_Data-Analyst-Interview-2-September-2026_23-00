b0d71991b1c7add462c474e18678b1fa25ca6fdf
HaGa Analytics <nengkya@yahoo.com>
2026-08-16 05:14:03 +0700

    https://www.geeksforgeeks.org/data-analysis/data-analysis-tutorial/

---
+++ DataTypes.py
@@ -0,0 +1,149 @@
+'''
+Data and its Types
+
+Data is the raw form of information, a collection of facts, figures, symbols or observations that represent details about events, objects or phenomena.
+By itself, data may appear meaningless, but when organized, processed and interpreted, it transforms into valuable insights that support decision-making, problem-solving and innovation.
+
+Data refers to raw facts, figures, or information that can be processed and analysed to extract meaningful insights.
+In data science and computing, data is categorised into different types based on its structure and nature.
+Understanding its type helps in selecting appropriate analysis and processing methods.
+
+Types of Data
+Data can be categorised in different ways depending on how it is collected, stored and represented. Broadly, it falls into the following types:
+
+
+1. Quantitative Data
+
+   Quantitative data is information that can be measured, counted and expressed in numerical form.
+   It provides objective values that can be analyzed statistically to identify patterns, trends and relationships.
+
+   - Represents numbers and measurable values.
+   - Can be divided into: Discrete data (Whole numbers) and Continuous data (Values on a scale).
+   - Widely used in research, finance, engineering and business analytics.
+
+   Example: Age of people, number of customers visiting a store, temperature readings, sales revenue.
+
+
+2. Qualitative Data
+
+   Qualitative data is descriptive, non-numeric information that explains qualities, characteristics or categories rather than quantities. It helps understand opinions, experiences and meanings behind behaviors.
+
+   - Focuses on qualities, attributes and categories rather than numbers.
+   - Often collected through surveys, interviews or observations.
+   - Useful for understanding opinions, motivations and behaviors.
+
+   Example: Customer feedback (“satisfied”, “unsatisfied”), product colors, interview transcripts, social media comments.
+
+
+3. Structured Data
+   Structured data is information organized into a predefined format (rows and columns) that makes it easily searchable and manageable by traditional databases.
+
+   - Stored in relational databases or spreadsheets.
+   - Easy to process with SQL and other tools.
+   - Best suited for tasks requiring accuracy and consistency.
+
+   Example: Bank transactions, employee records, product inventories.
+
+
+4. Unstructured Data
+
+   Unstructured data is raw information that does not follow a predefined structure or format making it harder to organize and analyze with conventional tools.
+
+
+5. Semi-Structured Data
+
+   Semi-structured data combines aspects of structured and unstructured data. It does not reside in traditional tables but still contains tags or markers that provide a loose structure.
+
+   - Provides a balance between flexibility and structure.
+   - Easier to analyze than unstructured data, but less rigid than structured data.
+   - Often used in web applications, IoT devices and log systems.
+
+   Example: JSON files, XML documents, NoSQL databases, sensor logs.
+
+
+a. Big Data
+
+   When datasets grow in size, complexity and speed, traditional methods don’t suffice.
+   Big Data refers to datasets that are too large, too varied or too fast to be handled by traditional data processing tools.
+
+   The defining characteristics often called the Vs of Big Data are:
+
+   1. Volume   : Massive amounts of data.
+   2. Velocity : Speed of generation and processing.
+   3. Variety  : Different formats: structured, unstructured, semi-structured.
+   4. Veracity : Accuracy, trustworthiness of data to deal with noise and errors.
+   5. Value    : The usefulness of data i.e. having data is not enough, we must extract value from data.
+
+
+b. Data Collection
+
+   Data collection is the process of acquiring data from various sources and in diverse formats for the purpose of storage, analysis and insight generation. It’s often the first step in the data life cycle.
+
+   - It helps ensure data is accurate and useful.
+   - Can be done manually or automatically using tools and software.
+   - Proper collection is the first step in making data meaningful.
+
+   Examples
+
+   - Collecting customer surveys, website clicks, sensor readings, social media posts.
+   - Bringing data from multiple sources into one place (databases, files, cloud).
+
+
+c. Data Management
+
+   Data management refers to all the practices, policies and technology used to collect, store organize, process, maintain and make data available in a secure, efficient and usable form.
+   It covers the full lifecycle from creation to disposal.
+
+   - Includes storing organizing and updating data.
+   - Ensures data quality, consistency and availability.
+   - Helps different teams access the right data for their work.
+
+   Examples
+   - Storing company records in databases.
+   - Cleaning and organizing data to remove errors.
+
+
+d. Data Security
+
+   Data security refers to protecting data against unauthorized access, corruption, theft, loss or misuse. It involves both technical controls and policy or governance measures.
+
+   - Protects privacy and prevents misuse of data.
+   - Builds trust with customers and stakeholders.
+   - Prevents financial loss and reputational damage.
+
+   Examples
+
+   - Using passwords and encryption to protect sensitive information.
+   - Limiting who can access customer records or financial data.
+
+
+e. Role of Data in AI
+
+   Data is fundamental to Artificial Intelligence (AI) and Machine Learning (ML). AI models are trained on data and their performance, fairness, and reliability depends heavily on the quality, relevance and appropriateness of that data.
+
+   - AI models depend on accurate, clean and relevant data.
+   - Poor or biased data can make AI give wrong or unfair results.
+   - Properly managed data improves AI accuracy and usefulness.
+
+   Examples
+   - Netflix uses viewing history to suggest shows.
+   - Self-driving cars use sensor data to navigate safely.
+
+
+f. Applications
+
+   - Business and Decision-Making: Organizations analyze data to identify trends, optimize operations and make informed decisions.
+   - Healthcare: Patient records and medical data help in accurate diagnoses, personalized treatments and predicting disease outbreaks.
+   - Finance: Banks and financial institutions rely on data for fraud detection, credit scoring and market analysis.
+   - Technology and AI: Data fuels artificial intelligence and is used in applications like natural language processing, image recognition and predictive models.
+   - Marketing and Customer Experience: Customer data enables targeted advertising, personalized offers and better engagement.
+
+
+g. Challenges
+
+   - Quality and Accuracy: If data is wrong or incomplete, the insights we get will also be wrong.
+   - Volume and Complexity: There is a huge amount of data in many formats, which can be hard to handle with normal tools.
+   - Privacy and Ethics: Personal data must be protected to avoid misuse and to follow laws like GDPR or CCPA.
+   - Bias and Fairness: If data reflects existing biases, AI or machine learning can make unfair decisions.
+   - Management Effort: Storing, processing and maintaining data can be costly and time-consuming.
+'''
